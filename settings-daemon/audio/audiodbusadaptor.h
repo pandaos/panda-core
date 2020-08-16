cca1f9cb4a946ccb428fa7e11565354809f2c649
rekols <revenmartin@gmail.com>
2020-08-16 08:25:33 +0800

    chore: add licence

--- settings-daemon/audio/audiodbusadaptor.h
+++ settings-daemon/audio/audiodbusadaptor.h
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2020 PandaOS Team.
+ *
+ * Author:     revenmartin <revenmartin@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef AUDIODBUSADAPTOR_H
 #define AUDIODBUSADAPTOR_H
 
@@ -10,7 +29,7 @@ class AudioDBusAdaptor : public QDBusAbstractAdaptor
     Q_CLASSINFO("D-Bus Interface", "org.panda.Audio")
 
 public:
-    AudioDBusAdaptor(AudioManager *parent) 
+    AudioDBusAdaptor(AudioManager *parent)
       : QDBusAbstractAdaptor(parent),
         m_manager(parent)
     {
